b4faf0b20d1eecf2a06c2dc85b65b46630ae8bf8
zaid <zaidhusseini@gmail.com>
2018-10-12 18:41:33 -0700

    Adds two function implementations for recrurtion

--- src/recursion.js
+++ src/recursion.js
@@ -7,11 +7,23 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  
+  if (n < 0) return null;
+
+  if (n === 1 || n === 0){
+    return 1;
+  }
+
+  return n * factorial(n-1);
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+
+  if (array.length === 0) return 0;
+
+  return array[0] + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
@@ -21,6 +33,11 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+
+  if (n === 2 || n === 0) return true;
+  if (n === 1) return false;
+
+  return isEven(Math.floor(n/2));
 };
 
 // 5. Sum all integers below a given integer.
